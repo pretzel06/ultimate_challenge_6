82942886bb5e04e7a84b805b299e37b9db44b6fe
lester <lester.lamsen@gmail.com>
2018-03-17 17:27:06 -0500

    added a customer controller
    added a customer routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'customers', to: 'customers#index'
+  # get 'customers/:id', to: 'customers#show'
+  resources :customers, only: [:index, :show]
+  root to: 'customers#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
