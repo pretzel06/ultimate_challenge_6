840a27cc0dace4a9cbecce82bc394a07dd5b1f65
lester <lester.lamsen@gmail.com>
2018-03-17 18:08:14 -0500

    customer methods

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,18 @@
 class CustomersController < ApplicationController
   def index
+    @customers = Customers.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
+  end
+  end
+
+  def alphabetized
+    @customers = Customers.order(:Full_name)
+  end
+
+  def missing_email
+    @customer = Customer.find(params[:Email_address] == '')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   # get 'customers', to: 'customers#index'
   # get 'customers/:id', to: 'customers#show'
-  resources :customers, only: [:index, :show]
+  resources :customers, only: [:index, :show, :alphabetized, :missing_email]
+
+  resources :alphabetized do
+    member do
+      get '/customers/alphabetized'
+    end
+  end
+
+  resources :missing_email do
+    member do
+      get '/customers/missing_email'
+    end
+  end
   root to: 'customers#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
